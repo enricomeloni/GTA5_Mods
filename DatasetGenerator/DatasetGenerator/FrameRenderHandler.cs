5a99196a4d9d24b0ec45273f6f99b4ac9d933339
enricomeloni <enrico-meloni@hotmail.it>
2019-01-27 11:53:41 +0100

    Show bounding rects also for nearby peds

--- DatasetGenerator/DatasetGenerator/FrameRenderHandler.cs
+++ DatasetGenerator/DatasetGenerator/FrameRenderHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,32 +16,39 @@ namespace DatasetGenerator
 
             var graphics = e.Graphics;
             
-            var ped = Game.LocalPlayer.Character;
-            Weapon wp = ped.Inventory.EquippedWeaponObject;
+            var me = Game.LocalPlayer.Character;
+            
 
+
+            List<Ped> nearbyPeds = new List<Ped>(me.GetNearbyPeds(16));
+            nearbyPeds.Add(me);
 
             //Vector2 spine2 = World.ConvertWorldPositionToScreenPosition(ped.GetBonePosition(PedBoneId.Spine2));
             //graphics.DrawFilledCircle(spine2, 5, Color.Orange);
 
-
-            if (wp && wp.IsVisible)
+            foreach (var ped in nearbyPeds)
             {
-                BoundingBox bb = BoundingBox.FromWeapon(wp);
-                BoundingRect br = bb.ToBoundingRect();
 
-                //BoundsDrawer.DrawBoundingBox(bb, graphics);
-                BoundsDrawer.DrawBoundingRect(br, graphics);
                 BoundingBox headBox = BoundingBox.FromHead(ped);
                 BoundingRect headRect = headBox.ToBoundingRect();
                 BoundsDrawer.DrawBoundingRect(headRect, graphics);
+                Weapon wp = ped.Inventory.EquippedWeaponObject;
+                if (wp && wp.IsVisible)
+                {
+                    BoundingBox bb = BoundingBox.FromWeapon(wp);
+                    BoundingRect br = bb.ToBoundingRect();
+
+                    //BoundsDrawer.DrawBoundingBox(bb, graphics);
+                    BoundsDrawer.DrawBoundingRect(br, graphics);
+                }
+
+                BoundingBox chestBox = BoundingBox.FromChest(ped);
+                //BoundsDrawer.DrawBoundingBox(chestBox, graphics);
+                BoundsDrawer.DrawBoundingRect(chestBox.ToBoundingRect(), graphics);
+
+                BoundingBox pedBox = BoundingBox.FromPed(ped);
+                //BoundsDrawer.DrawBoundingBox(pedBox, graphics);
             }
-
-            BoundingBox chestBox = BoundingBox.FromChest(ped);
-            //BoundsDrawer.DrawBoundingBox(chestBox, graphics);
-            BoundsDrawer.DrawBoundingRect(chestBox.ToBoundingRect(), graphics);
-
-            BoundingBox pedBox = BoundingBox.FromPed(ped);
-            //BoundsDrawer.DrawBoundingBox(pedBox, graphics);
 
         }
     }
