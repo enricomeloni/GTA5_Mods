e97c234618a18bae74856da3c6a15c27ee2e7f6a
enricomeloni <enrico-meloni@hotmail.it>
2019-01-27 11:42:01 +0100

    Extend Vector3 to have projection on screen space

--- DatasetGenerator/DatasetGenerator/ExtensionMethods.cs
+++ DatasetGenerator/DatasetGenerator/ExtensionMethods.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Rage;
+using Rage.Native;
 
 namespace DatasetGenerator
 {
@@ -14,6 +15,18 @@ namespace DatasetGenerator
         {
             Vector4 transformedVector4 = Vector3.Transform(vector3, orientation);
             return new Vector3(transformedVector4.X, transformedVector4.Y, transformedVector4.Z);
+        }
+
+        public static Vector2 ProjectToScreen(this Vector3 vector3)
+        {
+            float vector_x;
+            float vector_y;
+
+            //NativeFunction.Natives.xF9904D11F1ACBEC3(vector3.X, vector3.Y, vector3.Z, out vector_x, out vector_y);
+            //return new Vector2(vector_x, vector_y);
+
+
+            return World.ConvertWorldPositionToScreenPosition(vector3);
         }
     }
 }
