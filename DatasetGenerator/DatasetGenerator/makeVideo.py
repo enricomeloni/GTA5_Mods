e079b902c90710198b627edd3bd332c4b2154473
enricomeloni <enrico-meloni@hotmail.it>
2019-02-07 17:51:15 +0100

    Fix makeVideo to strip also utf-8 byte order marker
    BOM is automatically added by .NET and must be stripped manually. '\ufeff' is the character code for utf8 bom

--- DatasetGenerator/DatasetGenerator/makeVideo.py
+++ DatasetGenerator/DatasetGenerator/makeVideo.py
@@ -7,6 +7,7 @@ import cv2
 
 from os import listdir
 from os.path import isfile, join
+import codecs
 
 dataDir = sys.argv[1]
 
@@ -14,47 +15,47 @@ FFMpegWriter = manimation.writers['ffmpeg']
 metadata = dict(title='Movie Test')
 writer = FFMpegWriter(fps=15, metadata=metadata)
 
-bmpFiles = [join(dataDir,file) for file in listdir(dataDir) if isfile(join(dataDir,file)) and file.endswith(".bmp")]
+bmpFiles = [join(dataDir,file) for file in listdir(dataDir) if isfile(join(dataDir,file)) and file.endswith('.bmp')]
 bmpFiles.sort()
 
 fig = plt.figure(figsize=(14,14))
-with writer.saving(fig, "writer_test.mp4", 100):
+with writer.saving(fig, 'writer_test.mp4', 100):
     for bmpFile in bmpFiles:
         plt.clf()
-        print("Processing file " + bmpFile)
-        txtFile = bmpFile.replace(".bmp", ".txt")
+        print('Processing file ' + bmpFile)
+        txtFile = bmpFile.replace('.bmp', '.txt')
 
-        with open(txtFile) as txtFileStream:
+        with codecs.open(txtFile, 'r', 'utf-8') as txtFileStream:
             content = txtFileStream.readlines()
 
-        content = [x.strip() for x in content] 
-        
-        img = cv2.imread(bmpFile)
-        h,w = img.shape[:2]
+        content = [x.strip().replace('\ufeff', '') for x in content] 
+        print('CONTENT[0] is ' + str(len(content[0])))
+        if content[0]: #check that at least a row exists
+            img = cv2.imread(bmpFile)
+            h,w = img.shape[:2]
 
-        bbs = []
-        for element in content:
-                bb_info = element.split()
-                bb_width = float(bb_info[3])*float(w)
-                bb_height = float(bb_info[4])*float(h)
-                center_bb_x = float(bb_info[1])*float(w)
-                center_bb_y = float(bb_info[2])*float(h)
-                top_left_bb_x = center_bb_x - bb_width/2.0
-                top_left_bb_y = center_bb_y - bb_height/2.0
-                bb = [int(top_left_bb_x), int(top_left_bb_y), int(bb_width), int(bb_height)]
-                #print(bb)
-                bbs.append(bb)
+            bbs = []
+            for element in content:
+                    bb_info = element.split()
+                    bb_width = float(bb_info[3])*float(w)
+                    bb_height = float(bb_info[4])*float(h)
+                    center_bb_x = float(bb_info[1])*float(w)
+                    center_bb_y = float(bb_info[2])*float(h)
+                    top_left_bb_x = center_bb_x - bb_width/2.0
+                    top_left_bb_y = center_bb_y - bb_height/2.0
+                    bb = [int(top_left_bb_x), int(top_left_bb_y), int(bb_width), int(bb_height)]
+                    #print(bb)
+                    bbs.append(bb)
 
-        for bb in bbs:
-            cv2.rectangle(img, (bb[0],bb[1]), (bb[0]+bb[2], bb[1]+bb[3]), (255, 255, 0), 2)
+            for bb in bbs:
+                cv2.rectangle(img, (bb[0],bb[1]), (bb[0]+bb[2], bb[1]+bb[3]), (255, 255, 0), 2)
 
-        
         plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
         writer.grab_frame()
 
         #plt.close(fig)
 
-        print("File " + bmpFile + " processed")
+        print('File ' + bmpFile + 'processed')
 
 
 #print(bmpFiles)
