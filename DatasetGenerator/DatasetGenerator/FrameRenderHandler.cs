de18989cbcf0eae08d7a90b8442cec093a704b68
enricomeloni <enrico-meloni@hotmail.it>
2019-02-08 17:32:26 +0100

    Implement new method of saving dataset
    CurrentTick is a counter from 0 to TicksWithPause-1 and is incremented each game tick, returning to 0 each round. At CurrentTick == 1, the game is paused until TicksWithPause-2 is reached. At that point, screen content and objects content will be synchronized: a screenshot can be safely taken and bounding rects will be aligned to the screenshot. At TicksWithPause-1 the game is unpaused, letting the game progress for a tick. Then the cycle starts over.

--- DatasetGenerator/DatasetGenerator/FrameRenderHandler.cs
+++ DatasetGenerator/DatasetGenerator/FrameRenderHandler.cs
@@ -24,41 +24,94 @@ namespace DatasetGenerator
         private static int FrameID = 1;
         private static readonly DirectoryInfo DatasetDirectory = new DirectoryInfo("D:/dataset");
 
+        private static readonly int TicksWithPause = 5;
+        private static int CurrentTick = 0;
         public static void BoundingBoxGraphicHandler(object sender, GraphicsEventArgs e)
         {
             if (IsRecording)
             {
-                var me = Game.LocalPlayer.Character;
-
-                var nearbyPeds = new List<Ped>(me.GetNearbyPeds(5));
-
-                //nearbyPeds = new List<Ped>();
-                //nearbyPeds.Add(me);
-
-                
-
-                var detectedObjects = new List<DetectedObject>();
-
-                using (var disposableCamera = new DisposableCamera(DisposableCamera.DefaultScriptedCamera))
+                if (CurrentTick == 1)
                 {
-                    var camera = disposableCamera.Camera;
-                    camera.SetCameraValues(Utility.GetGameplayCameraValues());
+                    Game.IsPaused = true;
+                }
+                else if (CurrentTick == TicksWithPause - 1)
+                {
+                    Game.IsPaused = false;
+                }
+                else if(CurrentTick == TicksWithPause -2)
+                {
+                    var me = Game.LocalPlayer.Character;
 
-                    foreach (var ped in nearbyPeds)
+                    var nearbyPeds = new List<Ped>(me.GetNearbyPeds(5));
+
+                    nearbyPeds = new List<Ped>();
+                    nearbyPeds.Add(me);
+
+                    var detectedObjects = DetectObjects(nearbyPeds);
+
+                    //create screen snapshot
+                    var bitmap = GetBitmapFromScreen();
+
+                    var frameMetadata = new FrameMetadata(FrameID, bitmap, detectedObjects);
+                    frameMetadata.SaveToFolder(DatasetDirectory);
+                    ++FrameID;
+                }
+
+                CurrentTick = (CurrentTick + 1) % TicksWithPause;
+            }
+        }
+
+
+        private static Bitmap GetBitmapFromScreen()
+        {
+            Size resolution = Game.Resolution;
+            var bitmap = new Bitmap(resolution.Width, resolution.Height, PixelFormat.Format32bppArgb);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.CopyFromScreen(0, 0, 0, 0, resolution);
+            return bitmap;
+        }
+
+        private static List<DetectedObject> DetectObjects(List<Ped> peds)
+        {
+            var detectedObjects = new List<DetectedObject>();
+
+            using (var disposableCamera = new DisposableCamera(DisposableCamera.DefaultScriptedCamera))
+            {
+                var camera = disposableCamera.Camera;
+                camera.SetCameraValues(Utility.GetGameplayCameraValues());
+
+                foreach (var ped in peds)
+                {
+                    var headBox = new HeadBoundingBox(ped);
+                    if (headBox.ShouldDraw(camera))
                     {
-                        var headBox = new HeadBoundingBox(ped);
-                        if (headBox.ShouldDraw(camera))
-                        {
-                            var detectedHead = headBox.ToDetectedObject();
-                            detectedObjects.Add(detectedHead);
-                        }
+                        var detectedHead = headBox.ToDetectedObject();
+                        detectedObjects.Add(detectedHead);
+                    }
 
-                        var chestBox = new ChestBoundingBox(ped);
-                        if (chestBox.ShouldDraw(camera))
+                    var chestBox = new ChestBoundingBox(ped);
+                    if (chestBox.ShouldDraw(camera))
+                    {
+                        var detectedChest = chestBox.ToDetectedObject();
+                        detectedObjects.Add(detectedChest);
+                    }
+
+                    var weapon = ped.Inventory.EquippedWeaponObject;
+                    if (weapon && weapon.IsVisible)
+                    {
+                        var weaponBox = new WeaponBoundingBox(weapon);
+                        if (weaponBox.ShouldDraw(camera))
                         {
-                            var detectedChest = chestBox.ToDetectedObject();
-                            detectedObjects.Add(detectedChest);
+                            var detectedWeapon = weaponBox.ToDetectedObject();
+                            detectedObjects.Add(detectedWeapon);
                         }
+                    }
+                }
+            }
+
+            return detectedObjects;
+        }
+
         public static void StartRecording()
         {
             if (!IsRecording)
@@ -77,8 +130,6 @@ namespace DatasetGenerator
             }
         }
 
-                        var weapon = ped.Inventory.EquippedWeaponObject;
-                        if (weapon && weapon.IsVisible)
         public static void DebugGraphicHandler(object sender, GraphicsEventArgs e)
         {
             var me = Game.LocalPlayer.Character;
