fac42e814fccd4887ea43a61ff29175aa03e84c8
enricomeloni <enrico-meloni@hotmail.it>
2019-02-15 18:44:08 +0100

    Add Component abstract class
    To be inherited by DatasetAnnotator, PlayerFly e ScenarioCreator

---
+++ DatasetGenerator/DatasetGenerator/Component.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Rage;
+
+namespace DatasetGenerator
+{
+    public abstract class Component
+    {
+        protected GameFiber GameFiber { get; set; }
+        protected abstract void Main();
+        protected abstract void HandleKeyboardState();
+
+        protected Component()
+        {
+            GameFiber = new GameFiber(Main);
+            GameFiber.Start();
+        }
+    }
+}
